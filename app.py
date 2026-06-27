9a89f79d7a4fe718d1b2099702bd954883925e27
moonskumar14-dot <moonskumar14@gmail.com>
2026-06-27 17:01:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
+import os
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello from Tech VJ'
-
+@app.route("/")
+def home():
+    return "Bot is running on Linux/Railway", 200
 
 if __name__ == "__main__":
-    app.run()
-  
+    port = int(os.environ.get("PORT", "8080"))
+    app.run(host="0.0.0.0", port=port)
